675086d039cc699f4fd54e3784109cbfcf7e6a11
wecand0 <shorinvadim1@gmail.com>
2022-12-14 14:29:26 +0300

    a tiny gtest

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,11 +10,10 @@ TEST(DataTest, IsDecoded) {
     std::shared_ptr<Base91> base91 = std::make_shared<Base91>();
 
     std::string data{"Hello world!"};
+    auto encoded_91 = base91->encode(data);
+    auto decoded_91 = base91->decode(encoded_91);
 
-    std::string encoded_91 = base91->encode(data);
-    std::string decoded_91 = base91->decode(encoded_91);
-
-    ASSERT_EQ("Hello world!", base91->decode(encoded_91));
+    ASSERT_EQ("Hello world!", decoded_91);
 }
 
 int main(int argc, char **argv) {
