ffb627969745f6a7dc5127d93435aeb0aa469f9d
vadim <vadim@delta.com>
2025-08-26 10:32:24 +0000

    change py version main

--- python/main.py
+++ python/main.py
@@ -1,11 +1,7 @@
 import base91
 
 if __name__ == '__main__':
-    src = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a " \
-          "lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of " \
-          "knowledge, exceeds the short vehemence of any carnal pleasure."
-    e = base91.encode(src)
-    print(e)
-
-    d = base91.decode(e)
-    print(d)
+    encoded = base91.encode("Hello world")
+    decoded = base91.decode(encoded)
+    print("Encoded", encoded)
+    print("Decoded", decoded)
